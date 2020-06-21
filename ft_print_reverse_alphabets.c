1c7cdd3cd5988fc4a8643587f4f89a4bc2419d87
Sihle Dubazana <sdubazan@c4r14s10.wethinkcode.co.za>
2020-06-21 11:59:32 +0200

    my day02 work

---
+++ ft_print_reverse_alphabets.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabets.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdubazan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 09:56:10 by sdubazan          #+#    #+#             */
+/*   Updated: 2020/06/19 09:56:15 by sdubazan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//ft_putchar function
+
+void ft_putchar(char a)
+{
+	write(1,&a,1);
+}
+
+void ft_print_alphabet(void)
+{
+	char alp = 'z';
+	while(alp<='a')
+	{
+		ft_putchar(alp);
+		al--;
+	}
+}
+
